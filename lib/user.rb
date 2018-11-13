0e07195ce95a71dd89e3b44b209fc95bf0495884
leannemcabey <leanne.m.cabey@gmail.com>
2018-11-13 17:47:00 -0500

    further build out user class

--- lib/user.rb
+++ lib/user.rb
@@ -14,15 +14,31 @@ class User < ActiveRecord::Base
 
   def get_my_avg_genre_ratings
     my_animes = UsersAnime.all.select {|record| record.user == self}
+    ratings = {}
     # iterate through my_animes to create a hash of {genre => [rating, rating, rating, etc]}
     # each iteration should check to see if the genre is already a key, if it is, push the rating,
     # if it's not, add the key and the rating
+    my_animes.each do |record|
+      if ratings.keys.include?(record["genre_id"])
+        ratings[record["genre_id"]] << record["rating"]
+      else
+        ratings[record["genre_id"]] = []
+        ratings[record["genre_id"]] << record["rating"]
+      end
+    end
     # iterate through the hash using map and add the ratings and divide by the length of the value array
+    ratings.map do |genre_id, ratings|
+      sum = ratings.inject {|sum, rating| sum + rating}
+      avg = sum / ratings.length
+      {genre_id => avg}
+    end
   end
 
   def get_recommendations_by_my_ratings
-    # call get get_avg_genre_ratings
+    # call get_avg_genre_ratings
+    avgs = get_avg_genre_ratings
     # filter that down to any genres that have an average rating over x
+    avgs.select {|avg|}
     # run get_recommendations_by_genre for each of those genres
   end
 
