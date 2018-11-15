dda5acc93ac64af66561e63f5e02a7d1dd3989ab
leannemcabey <leanne.m.cabey@gmail.com>
2018-11-15 11:49:26 -0500

    fix recommendations method to exclude already watched animes

--- lib/user.rb
+++ lib/user.rb
@@ -3,19 +3,34 @@ class User < ActiveRecord::Base
   has_many :animes, through: :users_animes
 
   def get_recommendations_by_genre(genre_name)
+    my_anime_ids = self.animes.map {|anime| anime.id}
     if self.age < 18
-      recommendations = Anime.joins(:genre).where("genres.name = ? AND animes.age_rating IN ('G','PG')", genre_name).order('score DESC').limit(5)
+      recommendations = Anime
+                        .joins(:genre)
+                        .where("genres.name = ?
+                          AND animes.age_rating IN ('G','PG')
+                          AND animes.id NOT IN (?)", genre_name, my_anime_ids)
+                        .order('score DESC')
+                        .limit(5)
     else
-      recommendations = Anime.joins(:genre).where("genres.name = ?", genre_name).order('score DESC').limit(5)
+      recommendations = Anime
+                        .joins(:genre)
+                        .where("genres.name = ?
+                          AND animes.id NOT IN (?)", genre_name, my_anime_ids)
+                        .order('score DESC').limit(5)
     end
-    puts "Here are the top 5 animes in the #{genre_name} genre:"
+    puts "Here are the top 5 animes you haven't watched in the #{genre_name} genre:"
     recommendations.each_with_index do |rec, index|
       puts "#{index + 1}. #{rec.title} - Score: #{rec.score}"
     end
   end
 
   def get_my_avg_genre_ratings
-    big_decimal_avgs = UsersAnime.joins(anime: :genre).where("users_animes.user_id = ?", self.id).group('genres.name').average(:user_rating)
+    big_decimal_avgs = UsersAnime
+                       .joins(anime: :genre)
+                       .where("users_animes.user_id = ?", self.id)
+                       .group('genres.name')
+                       .average(:user_rating)
     big_decimal_avgs.map {|genre, big_decimal| {genre => big_decimal.truncate(2).to_f}}
   end
 
