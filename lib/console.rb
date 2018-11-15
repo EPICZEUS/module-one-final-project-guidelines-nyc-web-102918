d30889ceade1723f959c8ea27fdc1930664aa29a
EPICZEUS <s77broz@gmail.com>
2018-11-15 09:33:47 -0500

    Add data validation to add

--- lib/console.rb
+++ lib/console.rb
@@ -6,7 +6,7 @@ class Console
 		puts "`add`             - adds an anime to your watched list"
 		puts "`view`            - views current anime list"
 		puts "`recommendations` - displays your curated recommendations"
-		puts "`genre`.          - displays recommendations based on a genre"
+		puts "`genre`           - displays recommendations based on a genre"
 		puts "`exit`            - exits the program"
 	end
 
@@ -21,10 +21,13 @@ class Console
 	def user
 		puts "Please enter your name:"
 		name = self.input
-		puts "Please enter your age:"
-		age = self.input
+		age = nil
+		until age >= 13
+			puts "Please enter your age:"
+			age = self.input.to_i
+		end
 
-		@user = User.find_or_create_by(name: name, age: age.to_i)
+		@user = User.find_or_create_by(name: name, age: age)
 	end
 
 	def add
@@ -40,20 +43,28 @@ class Console
 		if animes.empty?
 			puts "No anime found by name: #{title}."
 			return
-		elsif animes.length > 1
+		else
 			animes.each_with_index do |anime, i|
 				puts "#{i + 1}. #{anime.title}"
 			end
-			puts "\nMultiple animes found, please specify by number:"
+			puts "\nPlease specify by number (0 to cancel):"
 			selection = self.input.to_i
 
+			return if selection == 0
+
 			anime = animes[selection - 1]
-		else
-			anime = animes.first
 		end
 
-		puts "Please enter your rating (1-5):"
-		rating = self.input.to_i
+		if @user.animes.include?(anime)
+			puts "Anime already in list."
+			return
+		end
+
+		rating = 0
+		until rating.between?(1,5)
+			puts "Please enter your rating (1-5):"
+			rating = self.input.to_i
+		end
 
 		UsersAnime.create(user_id: @user.id, anime_id: anime.id, user_rating: rating)
 	end
@@ -69,6 +80,10 @@ class Console
 	end
 
 	def recommendations
+		if @user.nil?
+			puts "Please enter your user first!"
+			return
+		end
 		@user.get_recommendations_by_my_ratings
 	end
 
